db5117fbf9ed5a24b27f84a0b9336b845683e7b6
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2024-05-09 15:38:24 +0700

    navigate noti

--- client/src/components/auction/NotiCard.tsx
+++ client/src/components/auction/NotiCard.tsx
@@ -1,13 +1,27 @@
 import { NotificationInterface } from "../../types/NotificationInterface";
+import { Link } from "react-router-dom"; // Đảm bảo bạn import Link từ react-router-dom
 
 const NotiCard = ({ noti, onDelete }: { noti: NotificationInterface; onDelete: (id: string) => void; }) => {
-  const { _id, from, content, created_at } = noti;
+  const { _id, from, content, created_at, type } = noti;
   const formattedCreatedAt = created_at ? created_at.toLocaleString() : "";
 
   const handleDelete = () => {
     const confirmDelete = window.confirm("Bạn có chắc chắn muốn xóa thông báo này?");
     if (confirmDelete) {
       onDelete(_id || "");
+    }
+  };
+
+  const goToLink = () => {
+    switch (type) {
+      case "duyetProvider":
+        return "/admin/providerList";
+      case "yeuCauXoa":
+        return "/admin/auctionlist";
+      case "yeuCauDuyet":
+        return "/admin/auctionlist";
+      default:
+        return "/";
     }
   };
 
@@ -22,6 +36,11 @@ const NotiCard = ({ noti, onDelete }: { noti: NotificationInterface; onDelete: (
       <div className="absolute top-0 right-0 mt-4 mr-4 text-[#506b94]">
         <span>Time: {formattedCreatedAt}</span>
         <button onClick={handleDelete} className="ml-2 text-red-600">Delete</button>
+        {type && (
+          <Link to={goToLink()}>
+            <button className="ml-2 mt-2 text-blue-600">Go to</button>
+          </Link>
+        )}
       </div>
 
       <div className="p-4 pt-10 mt-5">
