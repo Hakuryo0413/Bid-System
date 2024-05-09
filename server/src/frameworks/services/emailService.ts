72c19c2660eb2fb5b4d4064ac40ea5f391aee8db
chau4w <21020286@vnu.edu.vn>
2024-05-09 15:53:04 +0700

    Quen password

---
+++ server/src/frameworks/services/emailService.ts
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+import configKeys from "../../config";
+
+
+export const emailService = () => {
+    const transporter = nodemailer.createTransport({
+        service: configKeys.EMAIL_SERVICE || 'gmail',
+        auth: {
+            user: configKeys.EMAIL_USER,
+            pass: configKeys.EMAIL_PASSWORD,
+        }
+    });
+    
+    
+    let send_email = async (from_mail: string = configKeys.EMAIL_USER, to_mail: string, message: string, subject: string) => {
+        await transporter.sendMail(
+            {
+                from: from_mail,
+                to: to_mail,
+                subject: subject,
+                text: message,
+            }, (err: any) => {
+                if (err) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        );
+    }    
+  
+    return {
+        send_email,
+    };
+  };
+  
+  export type EmailService = typeof emailService;
