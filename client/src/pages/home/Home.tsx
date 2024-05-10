3e1058e9d9a439dc241fcf5fbb23fb487c881f2d
chau4w <21020286@vnu.edu.vn>
2024-05-11 01:00:24 +0700

    localhost:3000

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CommonHeader from '../../components/header/CommonHeader'
 import UserSideFooter from '../../components/footer/UserSideFooter'
+import HomePage from '../../components/user/home/Home';
 
 function Home() {
   return (
     <div>
         <CommonHeader/>
+        <HomePage/>
         <UserSideFooter/>
     </div>
   )
