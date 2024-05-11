211a5c1d2fbcadb4b6863a5fc61522acf401ad87
21020068_DoanThiMinhHang <48597930+mhjame@users.noreply.github.com>
2024-05-11 21:54:45 +0700

    fix upper_lower bug in HistoryAuctionPage

--- client/src/components/auction/HistoryAuctionCard.tsx
+++ client/src/components/auction/HistoryAuctionCard.tsx
@@ -7,15 +7,25 @@ const HistoryAunctionCard = ({
   historyAuction: HistoryInterface;
   cardState: String;
 }) => {
-  const {_id, sim, room, created_at } = historyAuction;
+  const {_id, sim, room, created_at,state} = historyAuction;
   const formattedCreatedAt = created_at ? created_at.toLocaleString() : "";
 
+  // console.log("hisAuctionCard", cardState);
   return (
     <tr>
       <th scope="row">{sim}</th>
       <td>{room}</td>
       <td>{formattedCreatedAt}</td>
-      {cardState === "chưa thanh toán" ? (
+      {cardState.toLocaleLowerCase() === "chưa thanh toán" ? (
+        <td>
+          <a className="button Bpay" href={`/user/payment/${room}/${sim}/${_id}`}>
+            Thanh toán
+          </a>
+          <a className="button BDpay" href={`/user/cancel/${room}/${sim}/${_id}`}>
+            Hủy thanh toán
+          </a>
+        </td>
+      ) : state?.toLocaleLowerCase() === "chưa thanh toán" ? (
         <td>
           <a className="button Bpay" href={`/user/payment/${room}/${sim}/${_id}`}>
             Thanh toán
